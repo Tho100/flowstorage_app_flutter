eeae89052668c53973fa5989f67d38c435ce4b35
Tho100 <nfrealyt@gmail.com>
2023-09-26 15:59:56 +0800

    refactor: Limit gallery upload based on storage space

--- lib/models/picker_model.dart
+++ lib/models/picker_model.dart
@@ -2,24 +2,35 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flowstorage_fsc/constant.dart';
 import 'package:flowstorage_fsc/global/globals.dart';
 import 'package:flowstorage_fsc/main.dart';
+import 'package:flowstorage_fsc/provider/storage_data_provider.dart';
 import 'package:flowstorage_fsc/provider/temp_data_provider.dart';
+import 'package:flowstorage_fsc/provider/user_data_provider.dart';
 import 'package:flowstorage_fsc/themes/theme_color.dart';
+import 'package:flowstorage_fsc/user_settings/account_plan_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:image_picker_plus/image_picker_plus.dart';
 
 class PickerModel {
 
+  final storageData = GetIt.instance<StorageDataProvider>();
+  final userData = GetIt.instance<UserDataProvider>();
+
   Future<SelectedImagesDetails?> galleryPicker({required ImageSource source}) async {
 
     try {
+
+      final maxUpload = AccountPlan.mapFilesUpload[userData.accountType];
+      final currentUpload = storageData.fileNamesFilteredList.length;
+
+      final maximumSelections = maxUpload!-currentUpload;
 
       ImagePickerPlus picker = ImagePickerPlus(navigatorKey.currentContext!);
       SelectedImagesDetails? details = await picker.pickBoth(
         source: source,
         multiSelection: true,
         galleryDisplaySettings: GalleryDisplaySettings(
-          maximumSelection: 25,
+          maximumSelection: maximumSelections,
           appTheme: AppTheme(
             focusColor: Colors.white, 
             primaryColor: ThemeColor.darkBlack,
