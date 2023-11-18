c768d4db8fa44c3e5e59b577a8aecab9331b8509
Tho100 <nfrealyt@gmail.com>
2023-11-18 20:45:32 +0800

    feature: Preview for searched PS file

--- lib/pages/public_storage/file_search_page.dart
+++ lib/pages/public_storage/file_search_page.dart
@@ -4,8 +4,10 @@ import 'package:flowstorage_fsc/connection/cluster_fsc.dart';
 import 'package:flowstorage_fsc/encryption/encryption_model.dart';
 import 'package:flowstorage_fsc/global/global_table.dart';
 import 'package:flowstorage_fsc/helper/get_assets.dart';
+import 'package:flowstorage_fsc/previewer/preview_file.dart';
 import 'package:flowstorage_fsc/provider/ps_storage_data.provider.dart';
 import 'package:flowstorage_fsc/provider/storage_data_provider.dart';
+import 'package:flowstorage_fsc/provider/temp_data_provider.dart';
 import 'package:flowstorage_fsc/provider/user_data_provider.dart';
 import 'package:flowstorage_fsc/themes/theme_color.dart';
 import 'package:flowstorage_fsc/themes/theme_style.dart';
@@ -37,6 +39,7 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
 
   final scrollListViewController = ScrollController();
 
+  final tempData = GetIt.instance<TempDataProvider>();
   final storageData = GetIt.instance<StorageDataProvider>();
   final userData = GetIt.instance<UserDataProvider>();
   final psStorageData = GetIt.instance<PsStorageDataProvider>();
@@ -75,6 +78,134 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
     );
   }
 
+  Widget buildOnEmpty() {
+    return const Expanded(
+      child: Center(
+        child: Center(
+          child: Text(
+            "It's empty here...",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(248, 94, 93, 93),
+              fontSize: 26,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSearchBar() {
+    return ResponsiveSearchBar(
+      controller: searchBarController,
+      focusNode: searchBarFocusNode, 
+      customWidth: 0.98,
+      visibility: null, 
+      hintText: "Enter a keyword", 
+      onChanged: (String value) {
+        //
+      }, 
+      filterTypeOnPressed: () {
+        //
+      }
+    );
+  }
+
+  Widget buildListView() {
+
+    const itemExtentValue = 90.0;
+    const bottomExtraSpacesHeight = 95.0;
+
+    return RawScrollbar(
+      radius: const Radius.circular(38),
+      thumbColor: ThemeColor.darkWhite,
+      minThumbLength: 2,
+      thickness: 2,
+      child: ListView.builder(
+        padding: const EdgeInsets.only(bottom: bottomExtraSpacesHeight),
+        itemExtent: itemExtentValue,
+        itemCount: titleList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onLongPress: () {
+              // Handle long press
+            },
+            onTap: () {
+              final fileType = fileNameList[index].split('.').last;
+              openSearchedFile(index, fileType);
+            },
+            child: Ink(
+              color: ThemeColor.darkBlack,
+              child: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.memory(base64.decode(imageBytesList[index]!),
+                    fit: BoxFit.cover, height: 65, width: 62
+                  ),
+                ),
+                title: Transform.translate(
+                  offset: const Offset(0, -6),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        titleList[index],
+                        style: const TextStyle(
+                          color: ThemeColor.justWhite,
+                          overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),              
+                      const SizedBox(height: 3),      
+                      Text(
+                        "Uploaded by ${uploaderNameList[index]}",
+                        style: const TextStyle(
+                          color: ThemeColor.justWhite,
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(uploadDateList[index],
+                        style: const TextStyle(color: ThemeColor.secondaryWhite, fontSize: 12.8),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildResultWidget() {
+
+    final mediaQuery = MediaQuery.of(context).size.height-180;
+
+    final verifySearching = titleList.isNotEmpty 
+      && searchBarController.text.isNotEmpty;
+
+    if (isSearchingForFile) {
+      return const LoadingIndicator();
+
+    } else if (verifySearching) {
+      return SizedBox(
+        height: mediaQuery,
+        child: buildListView(),
+      );
+
+    } else {
+      return buildOnEmpty();
+
+    } 
+
+  }
+
   Future<List<Map<String, String>>> getSearchedFileNameData(String keywordInput) async {
 
     const generalFileTableName = {
@@ -91,8 +222,8 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
 
     final conn = await SqlConnection.initializeConnection();
 
-    final queryImage = "SELECT CUST_TITLE, CUST_FILE, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM ps_info_image WHERE CUST_TITLE LIKE '$keywordInput%'";
-    final queryVideo = "SELECT CUST_TITLE, CUST_THUMB, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM ps_info_video WHERE CUST_TITLE LIKE '$keywordInput%'";
+    final queryImage = "SELECT CUST_TITLE, CUST_FILE, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM ps_info_image WHERE CUST_TITLE LIKE '%$keywordInput%'";
+    final queryVideo = "SELECT CUST_TITLE, CUST_THUMB, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM ps_info_video WHERE CUST_TITLE LIKE '%$keywordInput%'";
 
     final resultsImage = await conn.execute(queryImage);
     final resultsVideo = await conn.execute(queryVideo);
@@ -107,7 +238,7 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
     fileDataList.addAll(dataVideo);
 
     for(var tables in generalFileTableName) {
-      final query = "SELECT CUST_TITLE, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM $tables WHERE CUST_TITLE LIKE '$keywordInput%'";
+      final query = "SELECT CUST_TITLE, CUST_USERNAME, UPLOAD_DATE, CUST_FILE_PATH FROM $tables WHERE CUST_TITLE LIKE '%$keywordInput%'";
       final results = await conn.execute(query);
 
       final data = await processSearchingQuery(results);
@@ -197,143 +328,22 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
 
   }
 
-  Widget buildOnEmpty() {
-    return const Expanded(
-      child: Center(
-        child: Center(
-          child: Text(
-            "It's empty here...",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(248, 94, 93, 93),
-              fontSize: 26,
-            ),
-            textAlign: TextAlign.center,
-          ),
+  void openSearchedFile(int index, String fileType) {
+
+    final fileName = fileNameList[index];
+
+    tempData.setCurrentFileName(fileName);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => PreviewFile(
+          selectedFilename: fileName,
+          fileType: fileType,
+          tappedIndex: index
         ),
       ),
     );
-  }
-
-  Widget buildSearchBar() {
-    return ResponsiveSearchBar(
-      controller: searchBarController,
-      focusNode: searchBarFocusNode, 
-      customWidth: 0.98,
-      visibility: null, 
-      hintText: "Enter a keyword", 
-      onChanged: (String value) {
-        //
-      }, 
-      filterTypeOnPressed: () {
-        //
-      }
-    );
-  }
-
-  Widget buildListView() {
-
-    const itemExtentValue = 90.0;
-    const bottomExtraSpacesHeight = 95.0;
-
-    return RawScrollbar(
-      radius: const Radius.circular(38),
-      thumbColor: ThemeColor.darkWhite,
-      minThumbLength: 2,
-      thickness: 2,
-      child: ListView.builder(
-        padding: const EdgeInsets.only(bottom: bottomExtraSpacesHeight),
-        itemExtent: itemExtentValue,
-        itemCount: titleList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onLongPress: () {
-              // Handle long press
-            },
-            onTap: () {
-            },
-            child: Ink(
-              color: ThemeColor.darkBlack,
-              child: ListTile(
-                leading: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.memory(base64.decode(imageBytesList[index]!),
-                    fit: BoxFit.cover, height: 65, width: 62
-                  ),
-                ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: List.generate(
-                    1, 
-                    (index) => GestureDetector(
-                      onTap: () => _callBottomTrailing(index),
-                      child: const Icon(Icons.more_vert, color: Colors.white),
-                    ),
-                  ),
-                ),
-                title: Transform.translate(
-                  offset: const Offset(0, -6),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        titleList[index],
-                        style: const TextStyle(
-                          color: ThemeColor.justWhite,
-                          overflow: TextOverflow.ellipsis,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                        ),
-                      ),              
-                      const SizedBox(height: 3),      
-                      Text(
-                        "Uploaded by ${uploaderNameList[index]}",
-                        style: const TextStyle(
-                          color: ThemeColor.justWhite,
-                          overflow: TextOverflow.ellipsis,
-                          fontSize: 14,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(uploadDateList[index],
-                        style: const TextStyle(color: ThemeColor.secondaryWhite, fontSize: 12.8),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget buildResultWidget() {
-
-    final mediaQuery = MediaQuery.of(context).size.height-180;
-
-    final verifySearching = titleList.isNotEmpty 
-      && searchBarController.text.isNotEmpty;
-
-    if (isSearchingForFile) {
-      return const LoadingIndicator();
-
-    } else if (verifySearching) {
-      return SizedBox(
-        height: mediaQuery,
-        child: buildListView(),
-      );
-
-    } else {
-      return buildOnEmpty();
-
-    } 
-
-  }
-
-  void _callBottomTrailing(int index) {
 
   }
 
@@ -360,6 +370,7 @@ class FileSearchPagePsState extends State<FileSearchPagePs> {
       titleList.add(fileData['title']);
       uploadDateList.add(fileData['upload_date']);
       uploaderNameList.add(fileData['uploader_name']);
+      fileNameList.add(fileData['file_name']);
       imageBytesList.add(fileData['image']);
     }
 
