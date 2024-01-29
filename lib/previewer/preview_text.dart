fa6c721fd61f8f85d1df3ed5e4cdbbda189e296e
Tho100 <nfrealyt@gmail.com>
2024-01-29 21:29:35 +0800

    Asked user to discard changes on exit when changes is made for text-previewer

--- lib/previewer/preview_text.dart
+++ lib/previewer/preview_text.dart
@@ -5,6 +5,7 @@ import 'package:flowstorage_fsc/constant.dart';
 import 'package:flowstorage_fsc/global/global_table.dart';
 import 'package:flowstorage_fsc/global/globals.dart';
 import 'package:flowstorage_fsc/helper/call_preview_file_data.dart';
+import 'package:flowstorage_fsc/interact_dialog/text_dialog/discard_changes_dialog.dart';
 import 'package:flowstorage_fsc/models/offline_model.dart';
 import 'package:flowstorage_fsc/provider/temp_data_provider.dart';
 import 'package:flowstorage_fsc/widgets/failed_load.dart';
@@ -20,6 +21,9 @@ class PreviewText extends StatelessWidget {
   const PreviewText({Key? key}) : super(key: key);
 
   static final textController = TextEditingController();
+
+  static bool isChangesSaved = false;
+  static bool isChangesMade = false;
 
   Future<Uint8List> callTextDataAsync() async {
 
@@ -51,43 +55,69 @@ class PreviewText extends StatelessWidget {
     }
   }
 
+  Future<bool> discardChangesConfirmation(BuildContext context) async {
+    return await DiscardChangesDialog()
+                    .buildConfirmationDialog(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Uint8List>(
-      future: callTextDataAsync(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {        
-          textController.text = utf8.decode(snapshot.data!);
-          return Padding(
-            padding: const EdgeInsets.only(left: 14.0),
-            child: TextFormField(
-              controller: textController,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              style: GoogleFonts.roboto(
-                color: const Color.fromARGB(255, 220, 220, 220),
-                fontWeight: FontWeight.w500,
-                fontSize: 17,
-              ),
-              decoration: const InputDecoration(
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.transparent),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.transparent),
-                ),
-              ),
-            ),
-          );
+    return WillPopScope(
+      onWillPop: () async {
 
-        } else if (snapshot.hasError) {
-          return const FailedLoad();
+        if(isChangesMade && !isChangesSaved) {
+          isChangesMade = false;
+          isChangesSaved = false;
+          return await discardChangesConfirmation(context);
 
         } else {
-          return const LoadingIndicator();
-
+          isChangesMade = false;
+          isChangesSaved = false;
+          return true;
+          
         }
+
       },
+      child: FutureBuilder<Uint8List>(
+        future: callTextDataAsync(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {        
+            textController.text = utf8.decode(snapshot.data!);
+            return Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: TextFormField(
+                onChanged: (text) {
+                  isChangesMade = true;
+                  isChangesSaved = false;
+                },
+                controller: textController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                style: GoogleFonts.roboto(
+                  color: const Color.fromARGB(255, 220, 220, 220),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 17,
+                ),
+                decoration: const InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                ),
+              ),
+            );
+    
+          } else if (snapshot.hasError) {
+            return const FailedLoad();
+    
+          } else {
+            return const LoadingIndicator();
+    
+          }
+        },
+      ),
     );
   }
 
