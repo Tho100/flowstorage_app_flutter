c4de7c7a24ecdd111019f34a9f703460eab93c00
Tho100 <nfrealyt@gmail.com>
2024-02-07 13:10:13 +0800

    Remove directory when sort uplaod date is selected

--- lib/models/sorting_model.dart
+++ lib/models/sorting_model.dart
@@ -1,8 +1,6 @@
-import 'package:flowstorage_fsc/constant.dart';
 import 'package:flowstorage_fsc/helper/date_parser.dart';
 import 'package:flowstorage_fsc/provider/ps_storage_data.provider.dart';
 import 'package:flowstorage_fsc/provider/storage_data_provider.dart';
-import 'package:flowstorage_fsc/provider/temp_data_provider.dart';
 import 'package:flowstorage_fsc/themes/theme_style.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
@@ -11,7 +9,6 @@ class SortingModel {
 
   final storageData = GetIt.instance<StorageDataProvider>();
   final psStorageData = GetIt.instance<PsStorageDataProvider>();
-  final tempData = GetIt.instance<TempDataProvider>();
 
   String _formatDateTime(DateTime dateTime) {
 
@@ -35,55 +32,28 @@ class SortingModel {
 
     List<Map<String, dynamic>> itemList = [];
 
-    if(tempData.origin != OriginFile.public) {
-
-      for (int i = 0; i < storageData.fileNamesFilteredList.length; i++) {
-        itemList.add({
-          'file_name': storageData.fileNamesFilteredList[i],
-          'image_byte': storageData.imageBytesFilteredList[i],
-          'upload_date': DateParser(date: storageData.fileDateFilteredList[i]).parse(),
-        });
-      }
-
-    } else {
-
-      for (int i = 0; i < storageData.fileNamesFilteredList.length; i++) {
-        itemList.add({
-          'file_name': storageData.fileNamesFilteredList[i],
-          'image_byte': storageData.imageBytesFilteredList[i],
-          'upload_date': DateParser(date: storageData.fileDateFilteredList[i]).parse(),
-          'title': psStorageData.psTitleList[i],
-          'tag_value': psStorageData.psTagsList[i],
-          'uploader_name': psStorageData.psUploaderList[i]
-        });
-      }
-
-      psStorageData.psTitleList.clear();
-      psStorageData.psTagsList.clear();
-      psStorageData.psUploaderList.clear();
-
+    for (int i = 0; i < storageData.fileNamesFilteredList.length; i++) {
+      itemList.add({
+        'file_name': storageData.fileNamesFilteredList[i],
+        'image_byte': storageData.imageBytesFilteredList[i],
+        'upload_date': DateParser(date: storageData.fileDateFilteredList[i]).parse(),
+      });
     }
 
-    sortingIsAscendingUploadDate 
-    ? itemList.sort((a, b) => a['upload_date'].compareTo(b['upload_date']))
-    : itemList.sort((a, b) => b['upload_date'].compareTo(a['upload_date']));
+    itemList = itemList.where((item) => item['file_name'].contains('.')).toList();
+
+    sortingIsAscendingUploadDate
+        ? itemList.sort((a, b) => a['upload_date'].compareTo(b['upload_date']))
+        : itemList.sort((a, b) => b['upload_date'].compareTo(a['upload_date']));
 
     storageData.fileDateFilteredList.clear();
     storageData.fileNamesFilteredList.clear();
     storageData.imageBytesFilteredList.clear();
 
     for (final item in itemList) {
-
       storageData.fileNamesFilteredList.add(item['file_name']);
       storageData.imageBytesFilteredList.add(item['image_byte']);
       storageData.fileDateFilteredList.add(_formatDateTime(item['upload_date']));
-
-      if(tempData.origin == OriginFile.public) {
-        psStorageData.psTagsList.add(item['tag_value']);
-        psStorageData.psUploaderList.add(item['uploader_name']);
-        psStorageData.psTitleList.add(item['title']);
-      }
-
     }
 
     itemList.clear();
