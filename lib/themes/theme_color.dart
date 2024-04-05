f2161b1dccedc191bd46a7cd5764311eb4a54589
Tho100 <nfrealyt@gmail.com>
2024-04-05 16:51:33 +0800

    Updated theme color

--- lib/themes/theme_color.dart
+++ lib/themes/theme_color.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class ThemeColor {
 
-  static const darkBlack = Color.fromARGB(255, 5, 5, 5);
-  static const mediumBlack = Color.fromARGB(255, 14, 14, 14);
+  static const darkBlack = Color.fromARGB(255, 2, 2, 2);
+  static const mediumBlack = Color.fromARGB(255, 13, 13, 13);
 
-  static const darkGrey = Color.fromARGB(255, 17, 17, 17);
-  static const mediumGrey = Color.fromARGB(255, 26, 26, 26);
-  static const lightGrey = Color.fromARGB(255, 38, 38, 38);
+  static const darkGrey = Color.fromARGB(255, 16, 16, 16);
+  static const mediumGrey = Color.fromARGB(255, 25, 25, 25);
+  static const lightGrey = Color.fromARGB(255, 37, 37, 37);
 
   static const darkPurple = Color.fromARGB(255, 83, 6, 172);
   static const secondaryPurple = Color.fromARGB(255, 136, 71, 211);
