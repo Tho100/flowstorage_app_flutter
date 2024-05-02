a8cff429accf451b302e8d7bedb43610ab061818
Tho100 <nfrealyt@gmail.com>
2024-05-02 13:24:40 +0800

    Disable is edit all on toggle Photos

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:flowstorage_fsc/themes/theme_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import 'pages/splash_screen_page.dart';
